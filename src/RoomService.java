6eafebf5fa2c3be36a63034aca552753c3c63516
CodeWithGeorg <gkiarie816@gmail.com>
2026-02-17 22:24:15 +0300

    Added method comment in RoomService

--- src/RoomService.java
+++ src/RoomService.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class RoomService {
     ArrayList<Room> rooms = new ArrayList<>();
 
+    // Method to add a new room
     public void addRoom() {
         String no = JOptionPane.showInputDialog("Room number:");
         String type = JOptionPane.showInputDialog("Type:");
@@ -18,33 +19,33 @@ public class RoomService {
 
     public void viewRooms() {
         StringBuilder sb = new StringBuilder("=== ROOMS ===\n");
-        for(Room r : rooms){
+        for (Room r : rooms) {
             sb.append(r.id).append(" | ").append(r.roomNumber)
-              .append(" | ").append(r.type)
-              .append(" | ").append(r.price).append("\n");
+                    .append(" | ").append(r.type)
+                    .append(" | ").append(r.price).append("\n");
         }
         JOptionPane.showMessageDialog(null, sb.toString());
     }
 
- public void deleteRoom(String id){
-    rooms.removeIf(r -> r.id.equals(id));
-}
-
-public void updateRoom(String id) {
-    for (Room r : rooms) {
-        if (r.id.equals(id)) {
-            String newNumber = JOptionPane.showInputDialog("New Room number:", r.roomNumber);
-            String newType = JOptionPane.showInputDialog("New Type:", r.type);
-            double newPrice = Double.parseDouble(JOptionPane.showInputDialog("New Price:", r.price));
-
-            r.roomNumber = newNumber;
-            r.type = newType;
-            r.price = newPrice;
-            JOptionPane.showMessageDialog(null, "Room updated!");
-            return;
-        }
+    public void deleteRoom(String id) {
+        rooms.removeIf(r -> r.id.equals(id));
     }
-    JOptionPane.showMessageDialog(null, "Room ID not found!");
-}
+
+    public void updateRoom(String id) {
+        for (Room r : rooms) {
+            if (r.id.equals(id)) {
+                String newNumber = JOptionPane.showInputDialog("New Room number:", r.roomNumber);
+                String newType = JOptionPane.showInputDialog("New Type:", r.type);
+                double newPrice = Double.parseDouble(JOptionPane.showInputDialog("New Price:", r.price));
+
+                r.roomNumber = newNumber;
+                r.type = newType;
+                r.price = newPrice;
+                JOptionPane.showMessageDialog(null, "Room updated!");
+                return;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Room ID not found!");
+    }
 
 }
