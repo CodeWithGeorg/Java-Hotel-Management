8a03581bc3160da8dd3129caa979f99167e75367
CodeWithGeorg <gkiarie816@gmail.com>
2026-02-17 22:22:58 +0300

    Added constructor comment in Room class

--- src/Room.java
+++ src/Room.java
@@ -6,11 +6,12 @@ public class Room {
     public String type;
     public double price;
 
+    // Constructor to initialize room details
     public Room(String id, String roomNumber, String type, double price) {
         this.id = id;
         this.roomNumber = roomNumber;
         this.type = type;
         this.price = price;
     }
-    
+
 }
