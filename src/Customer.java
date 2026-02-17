a96ae61899933f1e4ea2daad3b7b425681074c69
CodeWithGeorg <gkiarie816@gmail.com>
2026-02-17 22:22:17 +0300

    Added constructor comment in Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -6,12 +6,12 @@ public class Customer {
     public String phone;
     public String roomNumber;
 
+    // Constructor to initialize customer details
     public Customer(String id, String name, String phone, String roomNumber) {
         this.id = id;
         this.name = name;
         this.phone = phone;
         this.roomNumber = roomNumber;
     }
-    
-}
 
+}
