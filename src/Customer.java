efb58f0710b476e461324cb9cc027748aae02577
CodeWithGeorg <gkiarie816@gmail.com>
2026-02-17 22:31:52 +0300

    Added field comments in Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,9 +1,13 @@
 package src;
 
 public class Customer {
+    // Unique customer identifier
     public String id;
+    // Customer full name
     public String name;
+    // Customer phone number
     public String phone;
+    // Room number assigned to customer
     public String roomNumber;
 
     // Constructor to initialize customer details
